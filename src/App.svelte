1b6915125010ca8824553719dba143bf79f1d422
Alex <alekiis.kun@gmail.com>
2021-07-21 00:28:18 -0300

    look's good

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,22 @@
-<script>
+<script lang="ts">
   import Lorem from './components/Lorem.svelte';
 
 	import Header from './components/Header.svelte';
 
 	import axios from 'axios';
 
-	const fetchLorem = async () => {
+	interface Lorem {
+		id: string;
+		author: string;
+		download_url: string;
+	}
+
+	const fetchLorem = async (): Promise<Lorem[]> => {
 		try {
 			const response = await axios.get('https://picsum.photos/v2/list');
-			return response;
+			return response.data;
 		}	catch(err){
 			console.log(err);
-			return err;
 		}
 	}
 </script>
@@ -19,23 +24,34 @@
 <Header />
 
 <main>
-	{#await fetchLorem() then response}
-		{#if response.data}
-			{#each response.data as lorem}
-				<Lorem/>
-			{/each}
-		{/if}
-	{/await}
+	<div class="list">
+		{#await fetchLorem() then list}
+			{#if list && list.length > 0}
+				{#each list as lorem}
+						<Lorem author={lorem.author} src={lorem.download_url}/>
+				{/each}
+			{/if}
+		{/await}
+	</div>
 </main>
 
 <style>
 	main {
 		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-items: center;
+	}
+
+	.list {
+		width: calc(100% - 20px);
 		padding: 24px;
-		margin: 12px;
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-		grid-row-gap: 20px;
-		grid-column-gap: 20px;
+		column-count: 2;
+		-moz-column-count: 2;
+		-webkit-column-count: 2;
+		-moz-column-gap: 30px;
+		-webkit-column-gap: 30px;
+		column-gap: 30px;
 	}
 </style>
